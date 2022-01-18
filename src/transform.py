5c19a58ad964521c6d40657082d1da29a3928864
Andres <softwareapgm@gmail.com>
2022-01-18 11:04:28 -0500

    Added comments

--- src/transform.py
+++ src/transform.py
@@ -8,10 +8,13 @@ Used this function when, you want to delete
 a character in a substring but not in the
 rest of the original string.
 
-text: original string
-start: start of subString
-end: end of subString
-char: char to delete, default is ','.
+Returns a string
+
+ -- PARAMETERS --
+text: 	original string
+start: 	start of subString
+end: 	end of subString
+char: 	char to delete, default is ','.
 
 '''
 def deleteInSubString(text, start, end, char=','):
@@ -24,7 +27,17 @@ def deleteInSubString(text, start, end, char=','):
 	return text
 
 
-#Get range of first pair of quotes. 
+'''
+Get the position of the Description Column.
+
+Loops through String and finds the first set
+of enclosing quotes.
+
+Returns array with initial and closing position.
+ 
+ -- PARAMETERS --
+txt: string to loop
+''' 
 def DescriptionColumn_Range(txt):
 	count = 0
 	pos=list()
@@ -35,6 +48,21 @@ def DescriptionColumn_Range(txt):
 		if count == 2:
 			return pos
 
+
+'''
+Adds a delimiter
+
+Returns a new string with the delimiter 
+added.
+
+-- PARAMETERS --
+text: 		string to be modified
+delimiter: 	char or string to be inserted
+flad:  		b - before target
+			a - after target
+target: 	substring where delimiter will be 
+		 	inserted
+'''
 def addDelimiter(text,delimiter,flag,target):
 	pos = text.find(target)
 	if not pos == -1:
@@ -45,10 +73,92 @@ def addDelimiter(text,delimiter,flag,target):
 			offset =  len(text[:pos])+len(target)
 			text = text[:offset+1]+delimiter+text[offset+1:]
 
-	return text 	
+	return text
 
+'''
+Clean up of Description Column
 
+Inital draft of data clean up on the 
+description column.
 
+Removal of extra commas and 'garbage' data
+
+Returns a string
+
+ -- PARAMETERS --
+data:	string 
+'''
+def clean_Description_Column(data):
+	#Replace data 00/00 for ,
+	data = re.sub("[0-9]{2}\/[0-9]{2}", ",", data)
+	
+	for i in ["'",",/20",",/21"]:
+		data = data.replace(i,"")
+
+	wordBank={
+		'c':["CREDITS","check","Check","CHARGE","CONSUMER"],
+		'd':["DEPOSIT","DEBITS"],
+		'f':["Fee","FEE","Funds"],
+		'o':["OVERDRAFT"],
+		'p':["PURCHASE","PAY","pymt","PMT","PMNT","Payment","PAYMENT","payment","PAYROLL"],
+		'r':["REFUND"],
+		't':["TAX","Transfer","transfer","TRANSFER"],
+		'w':["WITHDRWL","withdrawal","withdrwl"]
+	}
+
+	for k in wordBank:
+		for i in wordBank[k]:
+			i = i.lower()
+			if i in data:
+				data = addDelimiter(data,",", "b" , i)
+				data = addDelimiter(data,",", "a" , i)
+				#print(data)
+	
+	#Get Rid of repeating commas.
+	data = re.sub("#[0-9]+","",data)
+	data = re.sub(	'(,\s*,)',
+					',',
+					re.sub(	'(,{1,10}|,\s*,\b)', ",", data)
+				 )
+	
+	for match in re.finditer("\s[a-zA-Z]{2}$",data):
+		data = addDelimiter(data,',','b',data[match.start():match.end()+1])	
+	
+	return data
+
+'''
+Re-arranges nested list to become a 1-level list
+
+Descript column, item 1 in array, is a nested list
+items are moved one level up to become a single list
+and not a list of list.
+
+Returns a list
+
+ -- PARAMETERS --
+ 
+ data: list
+'''
+def addNewColumns(data):
+	newR = list()
+	for R in range(len(data)):
+		if R == 1:
+			for subr in data[R].split(","):
+				newR.append(subr)
+		else:
+			newR.append(data[R])
+	return newR
+
+'''
+Takes charge of initializing clean up data
+process.
+
+Returns the 'idea' of a clean dataFrame
+
+ -- PARAMETERS --
+
+ srcF:	path of raw file to clean up
+'''
 def cleanData(srcF):
 	dataframe = list()
 
@@ -60,54 +170,13 @@ def cleanData(srcF):
 			row = deleteInSubString(row, rg[0], rg[1], ';')
 			row = row.replace('"',"").split(',')
 			
-			#Replace data 00/00 for ,
-			row[1] = re.sub("[0-9]{2}\/[0-9]{2}", ",", row[1])
 			
-			for i in ["'",",/20",",/21"]:
-				row[1] = row[1].replace(i,"")
-
-				#Add , after PURCHASE
-
-			wordBank={
-				'c':["CREDITS","check","Check","CHARGE","CONSUMER"],
-				'd':["DEPOSIT","DEBITS"],
-				'f':["Fee","FEE","Funds"],
-				'o':["OVERDRAFT"],
-				'p':["PURCHASE","PAY","pymt","PMT","PMNT","Payment","PAYMENT","payment","PAYROLL"],
-				'r':["REFUND"],
-				't':["TAX","Transfer","transfer","TRANSFER"],
-				'w':["WITHDRWL","withdrawal"]
-			}
-
-			for k in wordBank:
-				for i in wordBank[k]:
-					if i in row[1]:
-						print()
-						print(row[1])
-						row[1] = addDelimiter(row[1],",", "b" , i)
-						row[1] = addDelimiter(row[1],",", "a" , i)
-						print(row[1])
-						print()
-
-			
+			row[1] = clean_Description_Column(row[1])
 			
 			row[3]=deleteInSubString(row[3],0,len(row[3]),"\n")
 			
-			#
-			newR = list()
-			for R in range(len(row)):
-				if R == 1:
-					for subr in row[R].split(","):
-						newR.append(subr)
-				else:
-					newR.append(row[R])
 			
-			#print(str(len(newR))+" : ",end="")
-			#print(newR)
-			
-			dataframe.append(newR)
-	
-	
+			dataframe.append(addNewColumns(row))
 
 
 	return dataframe
@@ -126,4 +195,4 @@ if __name__ == "__main__":
 	targetFile = "/home/delphinus/Devlp/WalletAnalysis/app/data/modify/modf.csv"
 
 	dataFrame = cleanData(sourceFile)
-	#saveToFile(dataFrame, targetFile)
+	saveToFile(dataFrame, targetFile)
